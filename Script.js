e8a3ed448a9f6fd91b0ad8caf6c22130d8f646ef
Bergaxolotl <minecrafter027@outlook.de>
2026-03-13 20:26:58 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,7 @@
+const input = document.querySelector(".textInput");
+const button = document.querySelector(".convertBtn");
+const output = document.querySelector(".output");
+
 function caesarEncrypt(text) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const shift = 3;
@@ -11,3 +15,13 @@ function caesarEncrypt(text) {
     })
     .join("");
 }
+button.addEventListener('click', () => {
+    const text = input.value.trim();
+    if(text === "") {
+        output.textContent = "Bitte Text eingeben!";
+    } else if (/^\d+$/.test(text)) {
+    output.textContent = "Ungültige Eingabe!";
+    } else {
+        output.textContent = caeserEncrypt(text);
+    }
+});
